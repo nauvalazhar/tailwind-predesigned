904c485a1eb91c2e6527cbff4181dd94e640bcd7
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2022-10-23 21:45:09 +0700

    (add) little open new tab button

--- frontend/components/Toolbar.js
+++ frontend/components/Toolbar.js
@@ -1,4 +1,4 @@
-import { DownloadBtn } from '@components';
+import { ExternalIcon, DownloadBtn } from '@components';
 import { modes as allModes, sizes, MODE_PREVIEW } from '@consts';
 import {
   useEditorContext,
@@ -9,7 +9,7 @@ import clsx from 'clsx';
 import { useDesign } from '@hooks';
 
 function Toolbar() {
-  const { data: design } = useDesign();
+  const { data: design, isLoading } = useDesign();
   const [{ mode, size }, dispatch] = useEditorContext();
 
   const modes = allModes.filter((m) => m.display !== false);
@@ -18,7 +18,7 @@ function Toolbar() {
     <div
       className={clsx(
         'bg-neutral-900 text-white py-3 px-5 border-b border-neutral-800 flex items-center transition-all',
-        !design && 'pointer-events-none opacity-60'
+        isLoading && 'pointer-events-none opacity-60'
       )}>
       <div className="w-4/12">
         <div className="flex items-center space-x-3">
@@ -44,22 +44,32 @@ function Toolbar() {
         </div>
       </div>
       <div className="w-4/12">
-        <div className="flex w-72 mx-auto">
-          {sizes.map((s) => (
-            <button
-              key={s.name}
-              className={clsx(
-                'w-10 h-10 text-sm flex-1 font-semibold first:rounded-tl first:rounded-bl last:rounded-tr last:rounded-br uppercase transition',
-                size.name === s.name
-                  ? 'bg-neutral-700'
-                  : 'bg-neutral-800 hover:bg-neutral-700',
-                mode !== MODE_PREVIEW && 'pointer-events-none opacity-60'
-              )}
-              type="button"
-              onClick={() => dispatch({ type: CHANGE_SIZE, payload: s })}>
-              {s.displayName}
-            </button>
-          ))}
+        <div className="flex justify-center">
+          <div className="flex">
+            {sizes.map((s) => (
+              <button
+                key={s.name}
+                className={clsx(
+                  'w-10 h-10 text-sm flex-1 font-semibold first:rounded-tl first:rounded-bl last:rounded-tr last:rounded-br uppercase transition',
+                  size.name === s.name
+                    ? 'bg-neutral-700'
+                    : 'bg-neutral-800 hover:bg-neutral-700',
+                  mode !== MODE_PREVIEW && 'pointer-events-none opacity-60'
+                )}
+                type="button"
+                onClick={() => dispatch({ type: CHANGE_SIZE, payload: s })}>
+                {s.displayName}
+              </button>
+            ))}
+          </div>
+          <a
+            href={design ? design.data.preview : ''}
+            target="_blank"
+            className="ml-4 bg-neutral-800 hover:bg-neutral-700 transition-all w-[40px] flex items-center justify-center rounded"
+            title="Open in a new tab"
+            rel="noreferrer">
+            <ExternalIcon className="w-4" />
+          </a>
         </div>
       </div>
       <div className="w-4/12 text-right">
